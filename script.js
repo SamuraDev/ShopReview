0245014626f6b271d0617700ca0f0092c20f85e4
SamuraDev <goydoyeoy@gmail.com>
2025-06-15 18:02:35 +0400

    Create script.js
    Signed-off-by: SamuraDev <goydoyeoy@gmail.com>

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".card button").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    alert("Спасибо за интерес! Эта функция скоро будет доступна 🛒");
+  });
+});
